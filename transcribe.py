acb1ad1f281af49011232f0e631ced018ad40bf4
ezou1 <eric.m.zou.2007@gmail.com>
2025-10-25 03:50:16 -0400

    transcribe.py is now functional, todo integrate

---
+++ transcribe.py
@@ -0,0 +1,21 @@
+from fish_audio_sdk import Session, ASRRequest
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+key = os.getenv("FISH_API_KEY")
+session = Session(key)
+
+audio = os.path.join("data", "main.mp3")
+text = os.path.join("data", "main.txt")
+
+# Read audio file
+with open(audio, "rb") as f:
+    audio_data = f.read()
+
+# Transcribe
+response = session.asr(ASRRequest(
+    audio=audio_data
+))
+with open(text, 'w') as f:
+    print(response.text, file=f)
