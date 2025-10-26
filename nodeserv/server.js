49fb52b767c985db1ff8d4662b51efc4f14134ed
eggoil166 <daniel.yi166@gmail.com>
2025-10-25 20:08:58 -0400

    limit change???

--- nodeserv/server.js
+++ nodeserv/server.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
